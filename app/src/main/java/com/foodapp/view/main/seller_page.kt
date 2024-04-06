daeb244cef755834312107d499105944999aa23c
Long Vu <longvu@Longs-MacBook-Pro.local>
2024-04-06 23:39:44 +0700

    [Build]: Handle ui for dashboard for seller page.

---
+++ app/src/main/java/com/foodapp/view/main/seller_page.kt
@@ -0,0 +1,21 @@
+package com.foodapp.view.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.foodapp.R
+
+class seller_page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seller_page)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
