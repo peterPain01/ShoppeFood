3d1f22cb59e0fed4ad534cf2ec1e1a48431f1432
pross1312 <dangvinhtuong12@gmail.com>
2024-04-10 12:09:11 +0700

    Feature: UI order review page

--- app/src/main/java/com/foodapp/view/adapter/CartListAdapter.kt
+++ app/src/main/java/com/foodapp/view/adapter/CartListAdapter.kt
@@ -3,8 +3,10 @@ package com.foodapp.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.foodapp.R
 import com.foodapp.data.model.DishItems
 
@@ -18,15 +20,25 @@ class CartListAdapter(private val dataList: List<DishItems>, private val res : I
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(dataList[position]);
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return dataList.size;
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        fun bind() {
+        private val image = itemView.findViewById<ImageView>(R.id.cart_item_imageView3)
+        private val name = itemView.findViewById<TextView>(R.id.cart_item_textView5)
+        private val price = itemView.findViewById<TextView>(R.id.cart_item_textView6)
+        private val count = itemView.findViewById<TextView>(R.id.cart_item_textView9)
+        fun bind(data: DishItems) {
+            Glide.with(itemView.context)
+                .load(data.dish.imageUrl)
+                .into(image)
+            name.text = data.dish.name
+            price.text = data.dish.price.toString()
+            count.text = data.count.toString()
         }
     }
 
