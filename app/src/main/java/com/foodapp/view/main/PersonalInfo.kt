b81e219a6824993128d6869989176f297591cce1
pross1312 <dangvinhtuong12@gmail.com>
2024-04-11 16:02:41 +0700

    Feature: Click event

--- app/src/main/java/com/foodapp/view/main/PersonalInfo.kt
+++ app/src/main/java/com/foodapp/view/main/PersonalInfo.kt
@@ -3,6 +3,7 @@ package com.foodapp.view.main
 import android.graphics.Paint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import com.foodapp.R
 
@@ -13,5 +14,9 @@ class PersonalInfo : AppCompatActivity() {
         setContentView(R.layout.activity_personal_info)
         edit_btn = findViewById<Button>(R.id.activity_personal_info_edit_button)
         edit_btn.paintFlags = edit_btn.paintFlags.or(Paint.UNDERLINE_TEXT_FLAG)
+    }
+
+    fun onClickedBack(view: View) {
+        finish()
     }
 }
