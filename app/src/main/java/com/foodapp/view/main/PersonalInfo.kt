09765532e57f861e57f299c476cb5133d204174a
pross1312 <dangvinhtuong12@gmail.com>
2024-04-11 15:44:24 +0700

    Feature: Personal info activity

---
+++ app/src/main/java/com/foodapp/view/main/PersonalInfo.kt
@@ -0,0 +1,17 @@
+package com.foodapp.view.main
+
+import android.graphics.Paint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import com.foodapp.R
+
+class PersonalInfo : AppCompatActivity() {
+    private lateinit var edit_btn: Button;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_personal_info)
+        edit_btn = findViewById<Button>(R.id.activity_personal_info_edit_button)
+        edit_btn.paintFlags = edit_btn.paintFlags.or(Paint.UNDERLINE_TEXT_FLAG)
+    }
+}
