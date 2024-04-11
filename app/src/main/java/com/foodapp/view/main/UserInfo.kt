c4cf6fe67fc7fb6b172138fc5c88979d4efd1bbb
pross1312 <dangvinhtuong12@gmail.com>
2024-04-11 16:02:41 +0700

    Feature: Click event

--- app/src/main/java/com/foodapp/view/main/UserInfo.kt
+++ app/src/main/java/com/foodapp/view/main/UserInfo.kt
@@ -1,12 +1,19 @@
 package com.foodapp.view.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.foodapp.R
 
 class UserInfo : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+    }
+
+    fun onClickedPersonalInfo(view: View) {
+        val new_intent = Intent(this, PersonalInfo::class.java);
+        startActivity(new_intent);
     }
 }
