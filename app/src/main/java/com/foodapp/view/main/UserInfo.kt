f0895c87e2f25c6e88481eda25921b5554addc4c
pross1312 <dangvinhtuong12@gmail.com>
2024-04-11 14:24:54 +0700

    Feature: UserInfo view

---
+++ app/src/main/java/com/foodapp/view/main/UserInfo.kt
@@ -0,0 +1,12 @@
+package com.foodapp.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.foodapp.R
+
+class UserInfo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_info)
+    }
+}
