19ae7242c4ecaa2ce5fc05bf899343261df91548
Long Vu <longvu@Longs-MacBook-Pro.local>
2024-04-16 23:23:25 +0700

    Fix

--- app/src/main/java/com/foodapp/view/main/seller_review.kt
+++ app/src/main/java/com/foodapp/view/main/seller_review.kt
@@ -1,11 +1,11 @@
 package com.foodapp.view.main
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.foodapp.R
@@ -13,62 +13,22 @@ import com.foodapp.utils.FakeData
 import com.foodapp.view.adapter.NotifyAdapter
 import com.foodapp.view.adapter.reviewAdapter
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [seller_review.newInstance] factory method to
- * create an instance of this fragment.
- */
-class seller_review : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+class seller_review : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_seller_review)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_seller_review, container, false);
         val dummyList = FakeData.createReview()
-
+        val BtnBack = findViewById<ImageButton>(R.id.seller_review_imageButton2)
         val adapter_vertical = reviewAdapter(dummyList, R.layout.item_review)
-        val recyclerView_vertical = view.findViewById<RecyclerView>(R.id.seller_review_list_notify)
-        recyclerView_vertical.layoutManager = GridLayoutManager(requireContext(), 1)
+        val recyclerView_vertical = findViewById<RecyclerView>(R.id.seller_review_list_notify)
+
+        recyclerView_vertical.layoutManager = GridLayoutManager(this, 1)
         recyclerView_vertical.adapter = adapter_vertical
 
-        return view;
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment seller_review.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            seller_review().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        BtnBack.setOnClickListener {
+            finish();
+        }
     }
 }
