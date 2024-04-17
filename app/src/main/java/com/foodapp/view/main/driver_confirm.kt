fb73762fea28adcad875cb55db85b2b5e1200100
Long Vu <longvu@Longs-MacBook-Pro.local>
2024-04-17 17:30:28 +0700

    Build: ui for driver.

--- app/src/main/java/com/foodapp/view/main/driver_confirm.kt
+++ app/src/main/java/com/foodapp/view/main/driver_confirm.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.foodapp.R
 
-class Driver_main : AppCompatActivity() {
+class driver_confirm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_driver_main)
+        setContentView(R.layout.activity_driver_confirm)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
