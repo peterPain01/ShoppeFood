2aff8389f8cad7e017be9ff89b6426983228965e
Long Vu <vucongleo@gmail.com>
2024-04-20 15:48:23 +0700

    Build: ui for manage restaurant.

--- app/src/main/java/com/foodapp/data/model/Restaurant.kt
+++ app/src/main/java/com/foodapp/data/model/Restaurant.kt
@@ -2,9 +2,6 @@ package com.foodapp.data.model
 
 import java.sql.Time
 
-// Danh sach mon an List<mon an>
-// Review
-// Voucher
 data class Restaurant(
     val id: String,
     var name: String,
---
+++ app/src/main/java/com/foodapp/view/adapter/MultipleChoiceSpinnerAdapter.kt
@@ -0,0 +1,42 @@
+package com.foodapp.view.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.CheckBox
+import android.widget.TextView
+import com.foodapp.R
+
+class MultipleChoiceSpinnerAdapter (context: Context, private val items: List<String>) :
+    ArrayAdapter<String>(context, android.R.layout.simple_spinner_dropdown_item, items) {
+
+    private var selectedItems = HashSet<Int>()
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return createView(position, convertView, parent)
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
+        return createView(position, convertView, parent)
+    }
+
+    private fun createView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val view = convertView ?: LayoutInflater.from(context).inflate(R.layout.item_spinner_multiple_choice, parent, false)
+        return view
+    }
+
+    fun toggleSelection(position: Int) {
+        if (selectedItems.contains(position)) {
+            selectedItems.remove(position)
+        } else {
+            selectedItems.add(position)
+        }
+        notifyDataSetChanged()
+    }
+
+    fun getSelectedItems(): Set<Int> {
+        return selectedItems
+    }
+}
