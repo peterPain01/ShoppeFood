9813fac109e5be3948b37c0f68402c0ababf6706
pross1312 <dangvinhtuong12@gmail.com>
2024-04-25 13:33:16 +0700

    Fix: Conflict

--- app/src/main/java/com/foodapp/data/model/Product.kt
+++ app/src/main/java/com/foodapp/data/model/Product.kt
@@ -1,5 +1,7 @@
 package com.foodapp.data.model
 
+import com.foodapp.data.NameDescImage
+
 data class Product(
     val _id: String,
     val product_sold: Int,
@@ -12,7 +14,14 @@ data class Product(
     val product_shop: String,
     val product_name: String,
     val product_price: Int,
-    val product_thumb: Int,
+    val product_thumb: String,
     val product_description: String,
     val product_comments: Array<String>
-)
+): NameDescImage {
+    override val name: String
+        get() = product_name
+    override val desc: String
+        get() = product_description
+    override val image: String
+        get() = product_thumb
+}
