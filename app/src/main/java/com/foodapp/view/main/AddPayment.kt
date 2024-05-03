351e5583e0d8650d63bba5a454480d11081cb0d0
21127617 <phamhuy1112003@gmail.com>
2024-05-03 13:10:25 +0700

    modify activities payment

---
+++ app/src/main/java/com/foodapp/view/main/AddPayment.kt
@@ -0,0 +1,17 @@
+package com.foodapp.view.main
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import com.foodapp.R
+
+class AddPayment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_payment)
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+}
