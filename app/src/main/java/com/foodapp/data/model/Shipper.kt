772decc3d8187eb69c959865b61a06077323e38a
21127617 <phamhuy1112003@gmail.com>
2024-05-05 19:10:03 +0700

    Modify Shipper Activity

---
+++ app/src/main/java/com/foodapp/data/model/Shipper.kt
@@ -0,0 +1,23 @@
+package com.foodapp.data.model
+
+import android.annotation.SuppressLint
+import com.google.gson.annotations.SerializedName
+import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.Date
+
+data class Shipper(
+    val _id: String = "",
+    var avatar: String = "",
+    var vehicle_image: String = "",
+    var license_plate_number: String = "",
+    var fullname: String = "Pham Hoang Gia Huy",
+    var isActive: Boolean = false   ,
+    var balance: Double = 0.0,
+    var device_token: String =
+    // only have lat lng
+//    var currentPosition: UserAddress? = null,
+
+
+
+    )
--- app/src/main/java/com/foodapp/view/main/driver_info.kt
+++ app/src/main/java/com/foodapp/view/main/driver_info.kt
@@ -1,16 +1,54 @@
 package com.foodapp.view.main
 
+import ApiService
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.foodapp.R
+import com.foodapp.data.model.auth.SessionManager
+import com.foodapp.data.repository.UserRepository
 
 class driver_info : AppCompatActivity() {
+    private lateinit  var backButton : ImageView
+    private lateinit var homeButton : ConstraintLayout
+    private lateinit var shipperName : TextView
+    private val userService = UserRepository(SessionManager(this)).create(ApiService::class.java)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_driver_info)
     }
+
+    override fun onStart() {
+        super.onStart()
+        backButton = findViewById(R.id.driver_info_back)
+        homeButton = findViewById(R.id.driver_info_home)
+        shipperName = findViewById(R.id.driver_info_name)
+        initEvent()
+        callApi()
+    }
+
+    private fun initEvent()
+    {
+        backButton.setOnClickListener{
+            this.finish()
+        }
+        homeButton.setOnClickListener {
+            val intent = Intent(this, MoneyMange::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun callApi()
+    {
+        userService.
+    }
+
 }
